6ca8a47276d66eb0731aa7783a2177bea715dc95
kapilkhub <31470043+kapilkhub@users.noreply.github.com>
2020-09-19 17:41:31 +0400

    added angular nav bar

--- future-shop/apps/kamazon/src/app/app.module.ts
+++ future-shop/apps/kamazon/src/app/app.module.ts
@@ -3,10 +3,11 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {SharedUiCommonModule} from '@kamazon/ui-common-lib'
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, BrowserAnimationsModule],
+  imports: [BrowserModule, BrowserAnimationsModule, SharedUiCommonModule],
   providers: [],
   bootstrap: [AppComponent],
 })
---
+++ future-shop/libs/shared/ui-common/src/lib/nav-bar/nav-bar.component.ts
@@ -0,0 +1,22 @@
+import { Component, ChangeDetectionStrategy } from '@angular/core';
+import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
+import { Observable } from 'rxjs';
+import { map, shareReplay } from 'rxjs/operators';
+
+@Component({
+  selector: 'kamazon-nav-bar',
+  templateUrl: './nav-bar.component.html',
+  styleUrls: ['./nav-bar.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush
+})
+export class NavBarComponent {
+
+  isHandset$: Observable<boolean> = this.breakpointObserver.observe(Breakpoints.Handset)
+    .pipe(
+      map(result => result.matches),
+      shareReplay()
+    );
+
+  constructor(private breakpointObserver: BreakpointObserver) {}
+
+}
