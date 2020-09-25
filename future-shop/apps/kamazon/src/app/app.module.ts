79c1824268fbbc19307f370050c133cd1e35c936
kapilkhub <31470043+kapilkhub@users.noreply.github.com>
2020-09-25 22:49:11 +0400

    commit for ngrx

--- future-shop/apps/kamazon/src/app/app.module.ts
+++ future-shop/apps/kamazon/src/app/app.module.ts
@@ -3,15 +3,36 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import {SharedUiCommonModule} from '@kamazon/ui-common-lib'
-import {AppRoutingModule} from "./app-routing.module"
-import {LayoutModule} from "@kamazon/layout-lib";
-import { HomeComponent } from './home/home.component'
-
+import { SharedUiCommonModule } from '@kamazon/ui-common-lib';
+import { AppRoutingModule } from './app-routing.module';
+import { LayoutModule } from '@kamazon/layout-lib';
+import { HomeComponent } from './home/home.component';
+import { StoreModule } from '@ngrx/store';
+import { EffectsModule } from '@ngrx/effects';
+import { StoreDevtoolsModule } from '@ngrx/store-devtools';
+import { environment } from '../environments/environment';
 
 @NgModule({
   declarations: [AppComponent, HomeComponent],
-  imports: [BrowserModule, BrowserAnimationsModule, SharedUiCommonModule,AppRoutingModule,LayoutModule],
+  imports: [
+    BrowserModule,
+    BrowserAnimationsModule,
+    SharedUiCommonModule,
+    AppRoutingModule,
+    LayoutModule,
+    StoreModule.forRoot(
+      {},
+      {
+        metaReducers: !environment.production ? [] : [],
+        runtimeChecks: {
+          strictActionImmutability: true,
+          strictStateImmutability: true,
+        },
+      }
+    ),
+    EffectsModule.forRoot([]),
+    !environment.production ? StoreDevtoolsModule.instrument() : [],
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
