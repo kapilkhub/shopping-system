4ec9769cfb49ee0befde9a344ed29c67b48b1198
kapilkhub <31470043+kapilkhub@users.noreply.github.com>
2020-09-26 13:28:16 +0400

    removed store management from feature library

--- future-shop/libs/products/feature/src/lib/products-feature.module.ts
+++ future-shop/libs/products/feature/src/lib/products-feature.module.ts
@@ -1,12 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
-import { StoreModule } from '@ngrx/store';
-import { EffectsModule } from '@ngrx/effects';
-import * as fromProducts from './+state/products.reducer';
-import { ProductsEffects } from './+state/products.effects';
-import { ProductsFacade } from './+state/products.facade';
-import { DataPersistence } from '@nrwl/angular';
 
 @NgModule({
   imports: [
@@ -15,14 +9,7 @@ import { DataPersistence } from '@nrwl/angular';
     RouterModule.forChild([
       /* {path: '', pathMatch: 'full', component: InsertYourComponentHere} */
     ]),
-
-    StoreModule.forFeature(
-      fromProducts.PRODUCTS_FEATURE_KEY,
-      fromProducts.reducer
-    ),
-
-    EffectsModule.forFeature([ProductsEffects]),
   ],
-  providers: [ProductsFacade, DataPersistence],
+  providers: [],
 })
 export class ProductsFeatureModule {}
